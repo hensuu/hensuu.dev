d50ff6655242c04cf3cf792f19f7a86ee2efdd36
hensuu <me@hensuu.dev>
2025-02-01 19:50:49 +0800

    Update welcome message (#2)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Welcome to hensuu.dev</h1>
+<a href="https://github.com/hensuu">GitHub</a>
