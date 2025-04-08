a69771f74baf6ef7f79e8f44a5e1a5814afe1fae
hensuu <me@hensuu.dev>
2025-04-08 23:22:08 +0800

    Initial Commit

---
+++ pages/blog.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Welcome to the Blog</h1>
+    <p>Here you can find the latest posts.</p>
+    <!-- Add a list of blog posts here -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BlogPage",
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #333;
+}
+</style>
