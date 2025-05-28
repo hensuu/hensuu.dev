d888821f0f3c7dce81fb40161416d7e3e21fef75
hensuu <me@hensuu.dev>
2025-05-28 17:50:58 +0800

    feat: Add plus symbol between roles

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="h-96 p-10">
+    <div class="h-110 p-10">
       <div class="flex flex-col gap-10 items-center justify-center h-full">
         <div class="flex flex-row items-center justify-center gap-2">
           <span class="text-4xl h-full font-medium text-sienna-400 mb-10">↘</span>
@@ -9,11 +9,12 @@
             class="ml-3 -mb-2 w-18 h-18 shrink-0 stroke-shamrock-600 stroke-[1.7] drop-shadow-md rotate-10"
           />
         </div>
-        <div class="flex flex-row flex-wrap items-center justify-center gap-2">
-          <div class="bg-sienna-50 border border-sienna-100 px-3 py-1 rounded-lg">
+        <div class="flex flex-col sm:flex-row items-center justify-center sm:gap-2">
+          <div class="bg-sienna-50 border border-sienna-200 px-3 py-1 rounded-lg">
             <span class="text-sienna-800 font-medium text-md">🧑‍💻 Developer</span>
           </div>
-          <div class="bg-sienna-50 border border-sienna-100 px-3 py-1 rounded-lg">
+          <span class="text-sienna-800 font-normal text-xl mb-0.5">+</span>
+          <div class="bg-sienna-50 border border-sienna-200 px-3 py-1 rounded-lg">
             <span class="text-sienna-800 font-medium text-md">🎵 Vocaloid Producer</span>
           </div>
         </div>
