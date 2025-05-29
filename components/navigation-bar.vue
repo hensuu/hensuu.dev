f312eb8368a4cc79cd9b834e24ac699aac45bad6
hensuu <me@hensuu.dev>
2025-05-29 16:55:14 +0800

    fix: Adjust padding and gap in navigation bar for improved layout

--- components/navigation-bar.vue
+++ components/navigation-bar.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex items-center justify-center ">
-    <div class="flex flex-row gap-2 items-center font-medium text-lg sm:text-xl stroke-[1.7]">
+    <div class="flex flex-row gap-1 md:gap-2 items-center font-medium text-lg sm:text-xl stroke-[1.7]">
       <div
-        class="rounded-full px-4 py-2 transition-all duration-200 cursor-pointer"
+        class="rounded-full px-3 py-2 transition-all duration-200 cursor-pointer"
         :class="{ 'bg-yellow-50 stroke-yellow-700 ring ring-yellow-400 stroke-[2] scale-105': $route.path === '/',
                   'hover:bg-yellow-50 hover:stroke-yellow-700 stroke-gray-600': $route.path !== '/',
         }"
@@ -11,14 +11,14 @@
         <LogoOutlined class="" />
       </div>
       <span
-        class="rounded-full px-4 py-2 transition-all duration-200 cursor-pointer"
+        class="rounded-full px-3 py-2 transition-all duration-200 cursor-pointer"
         :class="{ 'bg-violet-50 text-violet-700 ring ring-violet-400 scale-105': $route.path === '/blog',
                   'hover:bg-violet-50 hover:text-violet-700  text-gray-600': $route.path !== '/blog',
         }"
         @click="() => $router.push('/blog')"
       >blog</span>
-      <span class="hover:bg-gray-100 rounded-full px-4 py-2 transition-all duration-200 cursor-pointer">project</span>
-      <span class="hover:bg-gray-100 rounded-full px-4 py-2 transition-all duration-200 cursor-pointer">now</span>
+      <span class="hover:bg-gray-100 rounded-full px-3 py-2 transition-all duration-200 cursor-pointer">project</span>
+      <span class="hover:bg-gray-100 rounded-full px-3 py-2 transition-all duration-200 cursor-pointer">now</span>
     </div>
   </div>
 </template>
